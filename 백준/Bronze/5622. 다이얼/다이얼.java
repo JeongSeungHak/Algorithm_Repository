3fc36d2021639fd62d9dcd2921ea1cc37d13f4a1
JeongSeungHak <jeongsh0301@gmail.com>
2022-06-03 08:37:27 +0900

    [Bronze II] Title: 다이얼, Time: 204 ms, Memory: 17432 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,135 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String str1 = sc.next().toUpperCase();
+		
+		int time = 0;
+		
+		for(int i = 0; i < str1.length(); i++) {
+			switch (str1.charAt(i)) {
+			case 'A':
+				time += 3;
+				break;
+				
+			case 'B':
+				time += 3;
+				break;
+				
+			case 'C':
+				time += 3;
+				break;
+				
+				
+			case 'D':
+				time += 4;
+				break;
+				
+			case 'E':
+				time += 4;
+				break;
+				
+			case 'F':
+				time += 4;
+				break;
+				
+				
+			case 'G':
+				time += 5;
+				break;
+				
+			case 'H':
+				time += 5;
+				break;
+				
+			case 'I':
+				time += 5;
+				break;
+				
+				
+			case 'J':
+				time += 6;
+				break;
+				
+			case 'K':
+				time += 6;
+				break;
+				
+			case 'L':
+				time += 6;
+				break;
+				
+				
+			case 'M':
+				time += 7;
+				break;
+				
+			case 'N':
+				time += 7;
+				break;
+				
+			case 'O':
+				time += 7;
+				break;
+				
+				
+			case 'P':
+				time += 8;
+				break;
+				
+			case 'Q':
+				time += 8;
+				break;
+				
+			case 'R':
+				time += 8;
+				break;
+				
+			case 'S':
+				time += 8;
+				break;
+				
+			case 'T':
+				time += 9;
+				break;
+				
+			case 'U':
+				time += 9;
+				break;
+				
+			case 'V':
+				time += 9;
+				break;
+				
+			case 'W':
+				time += 10;
+				break;
+				
+			case 'X':
+				time += 10;
+				break;
+				
+			case 'Y':
+				time += 10;
+				break;
+				
+			case 'Z':
+				time += 10;
+				break;
+				
+
+			default:
+				break;
+			}
+		}
+		
+		System.out.println(time);
+		
+	}
+
+}
